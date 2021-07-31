d2370ae330bfc28b2c7bf00eeb2202973209c475
Sumit Aich <aich.1998@gmail.com>
2021-07-31 17:15:11 +0530

    test:add test

--- src/test/java/io/odpf/firehose/sink/mongodb/util/MongoSinkFactoryUtilTest.java
+++ src/test/java/io/odpf/firehose/sink/mongodb/util/MongoSinkFactoryUtilTest.java
@@ -35,6 +35,7 @@ public class MongoSinkFactoryUtilTest {
         config.put("SINK_MONGO_REQUEST_TIMEOUT_MS", "8277");
         config.put("SINK_MONGO_RETRY_STATUS_CODE_BLACKLIST", "11000");
         config.put("SINK_MONGO_MODE_UPDATE_ONLY_ENABLE", "true");
+        config.put("SINK_MONGO_AUTH_ENABLE", "false");
 
         mongoSinkConfig = ConfigFactory.create(MongoSinkConfig.class, config);
     }
@@ -52,7 +53,18 @@ public class MongoSinkFactoryUtilTest {
     public void shouldLogMongoSinkConfigWithCorrectMessage() {
 
         MongoSinkFactoryUtil.logMongoConfig(mongoSinkConfig, instrumentation);
-        verify(instrumentation, times(1)).logDebug("\n\tMONGO connection urls: localhost:8080\n\tMONGO DB name: myDb\n\tMONGO Primary Key: customer_id\n\tMONGO message type: JSON\n\tMONGO Collection Name: customers\n\tMONGO request timeout in ms: 8277\n\tMONGO retry status code blacklist: 11000\n\tMONGO update only mode: true\n\tMONGO should preserve proto field names: true");
+        verify(instrumentation, times(1))
+                .logDebug("\n\tMONGO connection urls: localhost:8080"
+                        + "\n\tMONGO Database name: myDb"
+                        + "\n\tMONGO Primary Key: customer_id"
+                        + "\n\tMONGO input message type: JSON"
+                        + "\n\tMONGO Collection Name: customers"
+                        + "\n\tMONGO request timeout in ms: 8277"
+                        + "\n\tMONGO retry status code blacklist: 11000"
+                        + "\n\tMONGO update only mode: true"
+                        + "\n\tMONGO Authentication Enable: false"
+                        + "\n\tMONGO Authentication Username: null"
+                        + "\n\tMONGO Authentication Database: null");
 
     }
 }
