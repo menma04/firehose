f27dd804b49b4fbfcdba97906050eebb51f612eb
Sumit Aich <aich.1998@gmail.com>
2021-08-05 10:58:31 +0530

    test: add tests for MongoSinkClientUtil

--- src/test/java/io/odpf/firehose/sink/mongodb/client/MongoSinkClientUtilTest.java
+++ src/test/java/io/odpf/firehose/sink/mongodb/client/MongoSinkClientUtilTest.java
@@ -1,12 +1,18 @@
 package io.odpf.firehose.sink.mongodb.client;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class MongoSinkClientUtilTest {
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Test
     public void shouldReturnBlackListRetryStatusCodesAsList() {
         String inputRetryStatusCodeBlacklist = "404, 502";
@@ -20,5 +26,37 @@ public class MongoSinkClientUtilTest {
         String inputRetryStatusCodeBlacklist = "";
         List<Integer> statusCodesAsList = MongoSinkClientUtil.getStatusCodesAsList(inputRetryStatusCodeBlacklist);
         assertEquals(0, statusCodesAsList.size());
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenStatusCodeNotAnInteger() {
+        String inputRetryStatusCodeBlacklist = "jahxh";
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Status code must be an integer");
+        MongoSinkClientUtil.getStatusCodesAsList(inputRetryStatusCodeBlacklist);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenAllStatusCodesNotAnInteger() {
+        String inputRetryStatusCodeBlacklist = "jahxh,rbtt,fbne";
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Status code must be an integer");
+        MongoSinkClientUtil.getStatusCodesAsList(inputRetryStatusCodeBlacklist);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenSomeOfTheStatusCodesNotAnInteger() {
+        String inputRetryStatusCodeBlacklist = "jahxh,608,wfrf";
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Status code must be an integer");
+        MongoSinkClientUtil.getStatusCodesAsList(inputRetryStatusCodeBlacklist);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenStatusCodesHaveInvalidCharacters() {
+        String inputRetryStatusCodeBlacklist = "608,++=$>#";
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Status code must be an integer");
+        MongoSinkClientUtil.getStatusCodesAsList(inputRetryStatusCodeBlacklist);
     }
 }
